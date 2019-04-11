ce0ef2e4e27e38a1e2747c98c989bf2162ab88fc
prembhusal <remp.bhusal@gmail.com>
2019-04-11 11:18:25 -0400

    Update SLINK2d.py

--- SLINK2d.py
+++ SLINK2d.py
@@ -5,18 +5,18 @@ from scipy.cluster.hierarchy import dendrogram, linkage,fcluster
 from matplotlib import pyplot as plt
 
 infile = open(sys.argv[1]) # input vector file with comma seperated values
-k = int(sys.argv[2])
+k = int(sys.argv[2]) # input k value to get max k cluster
 X = []
 for line in infile:
 	line = line.strip().split(",")
 	X.append([float(line[0]),float(line[1])])
 
-Z = linkage(X, 'single')
+Z = linkage(X, 'single') 
 fig = plt.figure(figsize=(25,10))
 dn = dendrogram(Z)
 plt.show()
 
-#to plot the points
+
 
 clusters = fcluster(Z, k, criterion='maxclust')
 for i in clusters:
