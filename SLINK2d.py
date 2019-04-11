df91db0ad547d203651f3f8e9f750b2a507dc0d7
prembhusal <remp.bhusal@gmail.com>
2019-04-11 11:17:03 -0400

    Update SLINK2d.py

--- SLINK2d.py
+++ SLINK2d.py
@@ -1,1 +1,23 @@
 
+import sys
+import numpy as np
+from scipy.cluster.hierarchy import dendrogram, linkage,fcluster
+from matplotlib import pyplot as plt
+
+infile = open(sys.argv[1])
+k = int(sys.argv[2])
+X = []
+for line in infile:
+	line = line.strip().split(",")
+	X.append([float(line[0]),float(line[1])])
+
+Z = linkage(X, 'single')
+fig = plt.figure(figsize=(25,10))
+dn = dendrogram(Z)
+plt.show()
+
+#to plot the points
+
+clusters = fcluster(Z, k, criterion='maxclust')
+for i in clusters:
+	print i
